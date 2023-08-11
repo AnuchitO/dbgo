01a63c94d397c924e7dab10b8a96e03dda931289
Nong AnuchitO <anuchit.prasertsang@gmail.com>
2023-08-11 10:54:42 +0700

    add

--- main.go
+++ main.go
@@ -51,6 +51,73 @@ func deleteWallet(db *sql.DB, walletID int) error {
 	return nil
 }
 
+type Wallet struct {
+	ID      int
+	Owner   string
+	Balance float64
+}
+
+func selectAllWallet(db *sql.DB) error {
+	query := `
+			SELECT * FROM wallet
+	`
+	rows, err := db.Query(query)
+	if err != nil {
+		return err
+	}
+	defer rows.Close()
+
+	var ws []Wallet
+
+	for rows.Next() {
+		var id int
+		var owner string
+		var balance float64
+		err = rows.Scan(&id, &owner, &balance)
+		if err != nil {
+			return err
+		}
+
+		wt := Wallet{
+			ID:      id,
+			Owner:   owner,
+			Balance: balance,
+		}
+
+		ws = append(ws, wt)
+
+		log.Println("id:", id, "owner:", owner, "balance:", balance)
+	}
+
+	log.Printf("wallets: %#v\n", ws)
+	return nil
+}
+
+func selectWalletById(db *sql.DB, walletID int) error {
+	query := `
+			SELECT * FROM wallet WHERE id = $1
+	`
+	row := db.QueryRow(query, walletID)
+
+	var id int
+	var owner string
+	var balance float64
+	err := row.Scan(&id, &owner, &balance)
+	if err != nil {
+		return err
+	}
+
+	wt := Wallet{
+		ID:      id,
+		Owner:   owner,
+		Balance: balance,
+	}
+
+	log.Println("id:", id, "owner:", owner, "balance:", balance)
+	log.Printf("wallet: %#v\n", wt)
+	return nil
+}
+
 func main() {
 	conn, err := sql.Open("postgres", "postgres://batsjuib:Voopy92tnjyUMBQhi0EDtxTdg--aA-rK@tiny.db.elephantsql.com/batsjuib")
 	if err != nil {
@@ -82,6 +149,16 @@ func main() {
 	err = updateWalletBalance(conn, 1, 200.00)
 	if err != nil {
 		log.Fatal("update error:", err)
+	}
+
+	err = selectAllWallet(conn)
+	if err != nil {
+		log.Fatal("select all error:", err)
+	}
+
+	err = selectWalletById(conn, 3)
+	if err != nil {
+		log.Fatal("select by id error:", err)
 	}
 
 	err = deleteWallet(conn, 1)
